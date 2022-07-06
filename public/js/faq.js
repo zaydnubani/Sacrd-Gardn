b0e7d09ab9e91ef4b378bd35832ea0c9465690f3
Zayd Nubani <zayd.nubani@gmail.com>
2022-07-06 16:06:27 -0400

    faq page

---
+++ public/js/faq.js
@@ -0,0 +1,25 @@
+const faq = document.getElementsByClassName('faq');
+
+for(let i=0;i<faq.length;i++){
+    const backgrounds = [
+        'B6.png'
+        ,
+        'B21.png'
+        ,
+        'B24.png'
+        ,
+        'B26.png'
+    ];
+
+    faq[i].children[0].style = `background-image: url('/images/backgrounds/body/${backgrounds[Math.floor(Math.random() * backgrounds.length)]}');`;
+
+    faq[i].children[0].children[1].addEventListener('click', () => {
+        faq[i].children[1].style = 'visibility: visibile; width:100%;';
+    });
+
+    faq[i].children[1].children[1].addEventListener('click', () => {
+        faq[i].children[1].style = 'visibility: hidden; width:5%;';
+    });
+
+};
+
