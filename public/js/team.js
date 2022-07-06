0abe8a805ca576641b2c40e796c58d4f61c199b2
Zayd Nubani <zayd.nubani@gmail.com>
2022-07-06 11:21:51 -0400

    team changes

--- public/js/team.js
+++ public/js/team.js
@@ -24,25 +24,28 @@ for(let i=0;i<bio.length;i++){
     bio[i].addEventListener('click', (event) => {
         console.log(screen.width);
         event.target.children[0].style = 'display: block;';
-        event.target.children[1].style = 'display: none;';
-        event.target.children[2].style = 'display: block;';
+        // event.target.children[1].style = 'display: none;';
+        event.target.children[1].style = 'display: block;';
+        // event.target.text = '';
 
         event.target.children[0].addEventListener('click', (poke) => {
             event.target.children[0].style = 'display: none;';
-            event.target.children[1].style = 'display: block;';
-            event.target.children[2].style = 'display: none;';
+            // event.target.children[1].style = 'display: block;';
+            event.target.children[1].style = 'display: none;';
             bio[i].style = 'width:10%; height:50%;';
         });
 
         if(screen.width >= 1400){
-            bio[i].style = `width:100%; height:100%; font-size:1.75rem; overflow-y:scroll; z-index:1; padding:2%;`;
+            bio[i].style = `width:100%; height:100%; font-size:4rem; overflow-y:scroll; z-index:1; padding:2%;`;
         } else if(screen.width >= 1200) {
-            bio[i].style = 'width:100%; height:100%; font-size:1.4rem; overflow-y:scroll; z-index:1; padding:1%';
+            bio[i].style = 'width:100%; height:100%; font-size:3rem; overflow-y:scroll; z-index:1; padding:1%';
         }  else if(screen.width >= 992) {
-            bio[i].style = 'width:100%; height:100%; font-size:1.2rem; overflow-y:scroll; z-index:1; padding:2%';
+            bio[i].style = 'width:100%; height:100%; font-size:2.5rem; overflow-y:scroll; z-index:1; padding:2%';
         } else if(screen.width >= 768) {
-            bio[i].style = 'width:100%; height:100%; font-size:1.2rem; overflow-y:scroll; z-index:1; padding-right:5%; padding-top:5%;';
+            bio[i].style = 'width:100%; height:100%; font-size:2rem; overflow-y:scroll; z-index:1; padding-right:5%;';
         } else if(screen.width >= 576) {
+            bio[i].style = 'width:100%; height:100%; font-size:1.5rem; overflow-y:scroll; z-index:1; padding:2%;';
+        } else if(screen.width >= 360) {
             bio[i].style = 'width:100%; height:100%; font-size:1rem; overflow-y:scroll; z-index:1; padding:2%;';
         }
     });
